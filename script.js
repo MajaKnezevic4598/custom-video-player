15a18ecd92edade77e9cacfa4d6bd9c1496d0c80
Maja Knezevic <maja.knezevic1@gmail.com>
2021-10-31 20:01:02 +0100

    responsive desing added

--- script.js
+++ script.js
@@ -4,68 +4,62 @@ const video = document.querySelector(".video");
 const innerVolumeRange = document.querySelector(".inner-volume-range")
 const playPauseBtn = document.querySelector(".play-pause-btn");
 const innerBar = document.querySelector(".inner-bar");
-console.dir(video);
 const curretnVideoDuration = document.querySelector(".curent-duration");
 const totalVideoDuration = document.querySelector(".total-duration");
 const toBeggining = document.querySelector(".toBegining");
 const skipButtons = document.querySelectorAll("[data-skip]");
-const volumeSlider = document.querySelector(".volume-slider");
-console.log(skipButtons)
 const volumConteiner = document.querySelector(".volume");
 const volumeImg = volumConteiner.querySelector(".fa-volume-up");
-console.log(volumeImg)
 const progresBar = document.querySelector(".progres-bar");
- let progressBarWidth = progresBar.clientWidth;
- const currentPlaybackRate = document.querySelector(".curent-playBackRate");
-
+let progressBarWidth = progresBar.clientWidth;
+const currentPlaybackRate = document.querySelector(".curent-playBackRate");
  const playbackRates = document.querySelectorAll("[data-playbackRate]");
- console.log(playbackRates)
+ const controls = document.querySelector(".controls");
+ const inputSlider = document.querySelector(".my-slider");
+ const toEnd = document.querySelector(".toEnd");
 
 let timer = null;
-/*kada zelimo da zavrsimo u stvari treba da pauziramo video i da namestimo video.currentTime = 0 */
 
-console.log(playPauseBtn);
-console.log(fullScreen);
 
-// function getFullScreenElement(){
-//     return document.fullscreenElement   
-//         || document.webkitFullscreenElement
-//         || document.mozFullscreenElement
-//         || document.msFullscreenElement
-// }
+function getFullScreenElement(){
+   
+    return document.fullscreenElement   
+        || document.webkitFullscreenElement
+        || document.mozFullscreenElement
+        || document.msFullscreenElement
 
-// function toggleFullScreen(){
-//     if(getFullScreenElement()){
-//         document.exitFullscreen();
-//     }
-//     else{
-//       videoConteiner.requestFullscreen().catch((e)=>{
-//             console.log(e)
-//         })
-//     }
-// }
-
-// fullScreen.addEventListener("click",toggleFullScreen);
-fullScreen.addEventListener("click",toggleFullScreen)
-
-function toggleFullScreen(){
-   if (video.requestFullscreen){
-       video.requestFullscreen()
-   } else if(video.webkitRequestFullScreen){
-       video.webkitRequestFullScreen()
-   } else if(video.mozRequestFullScreen){
-       video.mozRequestFullScreen()
-   }
 }
 
+function toggleFullScreen(){
+    if(getFullScreenElement()){
+        document.exitFullscreen();
+    }
+    else{
+      videoConteiner.requestFullscreen().catch((e)=>{
+            console.log(e)
+        })
+    }
+}
+
+fullScreen.addEventListener("click",toggleFullScreen);
+
+document.addEventListener("fullscreenchange",function(){
+  
+    if (document.fullscreenElement){
+            controls.style.transform = 'translateY(100%) translateY(-6px)';  
+    }
+    else{
+         controls.style.transform = "translateY(0%)";
+    }
+});
+
 function togglePlayPause(){
+
     if(video.paused){
-        // playPauseBtn.innerHTML = `<i class="far fa-pause-circle"></i>`;
         video.play();
         timer=setInterval(updateTime,100);
     }
     else if(!video.paused){
-        // playPauseBtn.innerHTML = `<i class="far fa-play-circle">`;
         video.pause();
     }
 }
@@ -102,20 +96,23 @@ function secondsToTime(time){
 }
 
 function updateTime(){
-    curretnVideoDuration.innerHTML = `Time: ${secondsToTime(video.currentTime)}`;
-    totalVideoDuration.innerHTML = `Total: ${secondsToTime(video.duration)}`
+    curretnVideoDuration.innerHTML = `${secondsToTime(video.currentTime)}`;
+    totalVideoDuration.innerHTML = `/ ${secondsToTime(video.duration)}`
 }
 
 toBeggining.addEventListener("click",function(){
-    clearInterval(timer);
-     curretnVideoDuration.innerHTML = `Time:00:00`;
-     video.pause();
      video.currentTime =0;
+     updateTime();
+     
 
 });
+toEnd.addEventListener("click",function(){
+  video.currentTime = video.duration;
+  video.pause();
+})
+
 
 function skip(){
-   console.log(this.dataset.skip)
    video.currentTime += parseFloat(this.dataset.skip);
 }
 
@@ -124,46 +121,30 @@ function skip(){
  });
 
 
- 
-
-//   volumeSlider.addEventListener("mousemove", handelRangeUpdate);
-  //ovaj event ne radi
-//ako nije mutirano onda je false
   function muteUnmute(){
-        console.log(this)
         if(video.muted){
             video.muted =false;
            this.className = "fas fa-volume-up";
-           volumeSlider.value = 100;
-           
+           inputSlider.value = 50;
+            slider();
         } else {
             this.className = "fas fa-volume-mute";
-           
               video.muted = true;
-              volumeSlider.value = 1;
+              inputSlider.value = 1;
+               slider();
         }
   }
 
   volumeImg.addEventListener("click",muteUnmute)
 
-
   function setVolume() {
-    console.log(this.value);
-   video.volume = volumeSlider.value/100
-    
+   video.volume = inputSlider.value/100;
   }
 
-  volumeSlider.addEventListener("change", setVolume);
-
- 
-//   const conteinerwidht = progresBar.widht;
-  console.dir(progresBar)
   progresBar.addEventListener("click",function(e){
        let clickPosition = e.clientX - this.getBoundingClientRect().left;
        let percentage = ((clickPosition / progressBarWidth) * 100).toFixed(2);
         innerBar.style.width = `${percentage}%`;
-        //treba i da postavimo video 
-        //imamo da je to procenat
         video.currentTime = (video.duration*percentage)/100;
   })
 
@@ -176,4 +157,15 @@ function skip(){
       })
   })
 
- 
+  inputSlider.addEventListener("input",slider)
+
+   const mySlider = document.querySelector(".my-slider");
+  
+
+   function slider() {
+     valPercent = (mySlider.value / mySlider.max) * 100;
+     mySlider.style.background = `linear-gradient(to right, #3baea0 ${valPercent}%, rgba(0,0,0,0.3) ${valPercent}%)`;
+    video.volume = inputSlider.value / 100;
+   }
+
+   slider();
